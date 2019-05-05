dea475d6a422dafa0a911bb37da6d043627a63dc
nessangynn <nguyenh22@spu.edu>
2019-05-04 17:39:30 -0700

    Second commit/ adding gender

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const hostname = 'leia.cs.spu.edu';
+const hostname = 'localhost';
 const port = 3022;
 
 var friends = require("./friends.json"); // Once for all times
@@ -15,14 +15,14 @@ const server = http.createServer((request, response) => {
         '               <meta http-equiv="X-UA-Compatible" content="IE=edge"> \n' +
         '               <meta name="viewport" content="width=device-width, initial-scale=1"> \n' +
         '               <meta name="description" content="Home Page"> \n' +
-        '               <meta name="author" content="Carlos Arias"> \n' +
+        '               <meta name="author" content="Vanessa Nguyen"> \n' +
         '               <title>Netcentric Computing Home Page</title> \n' +
         '               <!-- Bootstrap core CSS --> \n' +
 		'				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">\n' +
         '       </head> \n' +
         '        <body> \n' +
         '               <div class="container" style="text-align: center"> \n' +
-        '               <h1>Another Example of Node</h1><br> \n'
+        '               <h1>First Node Homework</h1><br> \n'
 
     );
 	var currentDate = new Date();
@@ -36,6 +36,7 @@ const server = http.createServer((request, response) => {
         '                                       <th scope="col">First Name</th> \n' +
         '                                       <th scope="col">Last Name</th> \n' +
         '                                       <th scope="col">Phone</th> \n' +
+        '                                       <th scope="col">Gender</th> \n' +
         '                               </tr> \n' +
         '                       </thead> \n' +
         '                       <tbody> \n'
@@ -46,7 +47,8 @@ const server = http.createServer((request, response) => {
 	                '                               <tr> \n' +
 	                '                                       <td>' + friends[key][f]["firstName"] + '</td> \n' +
 	                '                                       <td>' + friends[key][f]["lastName"] + '</td> \n' +
-	                '                                       <td>' + friends[key][f]["phone"] + '</td> \n' +
+                    '                                       <td>' + friends[key][f]["phone"] + '</td> \n' +
+                    '                                       <td>' + friends[key][f]["gender"] + '</td> \n' +
 	                '                               </tr> \n'
 	            );
 
